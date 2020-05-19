1478979a3064ba3f8d8dd0f4a9d448401d7574d8
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2020-05-19 22:20:31 +0530

    App bar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Appbar from './Components/AppBar'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello Redux
+        <Appbar />
+        
       </div>
     );
   }
