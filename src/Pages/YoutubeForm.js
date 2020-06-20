f8e02fbe1211f1feff2667e97cc1ec241d8ee3f9
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2020-06-21 01:54:37 +0530

    second form

--- src/Pages/YoutubeForm.js
+++ src/Pages/YoutubeForm.js
@@ -1,18 +1,20 @@
 import React from 'react'
-import {useFormik} from 'formik'
+import {Formik,Form,Field} from 'formik'
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import { Button } from '@material-ui/core';
 import { Alert } from '@material-ui/lab';
+import * as Yup from 'yup'
 
 const useStyles = makeStyles((theme) => ({
   root: {
     '& > *': {
       margin: theme.spacing(1),
-      width: '25ch',
+      width: '50ch',
       display:"flex",
       flexDirection:"column",
-      justifyContent:"center"
+      justifyContent:"center",
+
     },
   },
   main:{
@@ -23,40 +25,41 @@ const useStyles = makeStyles((theme) => ({
       alignItems:"center"
   }
 }));
+
+
+const initialValues={
+    name:'',
+    email:'',
+    channel:''
+}
+
+const validationSchema=Yup.object({
+    name:Yup.string().required('Required'),
+    email:Yup.string().email("Please provide a proper format").required('Required'),
+    channel:Yup.string().required('Required')
+})
+
 export default function YoutubeForm() {
     const classes = useStyles();
     
-    const formik = useFormik({
-        initialValues:{
-            name:'',
-            email:'',
-            channel:''
-        },
-        onSubmit:values=>{
-            console.log("submitted",values)
-        },
-        validate:values=>{
-            var errors={
-            }
-            if(!values.name){
-                formik.errors.name='Required'
-            }
-            if(!values.email){
-                formik.errors.email='Required'
-            }
-            if(!values.channel){
-                formik.errors.channel="Required"
-            }
-            return errors
-        }
+    // const formik = useFormik({
+    //     initialValues,
+    //     onSubmit:values=>{
+    //         console.log("submitted",values)
+    //     },
+    //     validationSchema
+    // })
+    // console.log('formik-touched',formik.touched)
+    // console.log(formik.errors)
 
 
-    })
-    console.log(formik.errors)
     return (
         <div className={classes.main}>
             <h1>Youtube Form</h1>
-            <form className={classes.root} noValidate autoComplete="off" onSubmit={formik.handleSubmit}>
+
+                {/* Without using formik method */}
+
+            {/* <form className={classes.root} noValidate autoComplete="off" onSubmit={formik.handleSubmit}>
                 <div>
                 <TextField 
                     id="name"
@@ -65,9 +68,10 @@ export default function YoutubeForm() {
                     label="Your Full Name" 
                     variant="outlined"
                     value={formik.values.name}
+                    onBlur={formik.handleBlur}
                     onChange={formik.handleChange}
                     />
-                    {formik.errors.name ? (
+                    {formik.touched.name && formik.errors.name ? (
                      <Alert severity="error">{formik.errors.name}</Alert>
 
                     ):(null)}
@@ -79,10 +83,11 @@ export default function YoutubeForm() {
                     label="Email Address" 
                     variant="outlined"
                     name="email"
+                    onBlur={formik.handleBlur}
                     value={formik.values.values}
                     onChange={formik.handleChange}
                      />
-                     {formik.errors.email ? (
+                     {formik.touched.email && formik.errors.email ? (
                      <Alert severity="error">{formik.errors.email}</Alert>
 
                     ):(null)}
@@ -93,17 +98,62 @@ export default function YoutubeForm() {
                     id="channel" 
                     label="Channel Name" 
                     variant="outlined"
+                    onBlur={formik.handleBlur}
                     name="channel"
                     value={formik.values.channel}
                     onChange={formik.handleChange} />
-                    {formik.errors.channel ? (
+                    {formik.touched.channel && formik.errors.channel ? (
                      <Alert severity="error">{formik.errors.channel}</Alert>
-
                     ):(null)}
                 
                 </div>
                 <Button type="submit"  variant="contained" color="secondary" fullWidth={true} >Submit</Button>   
-                </form>
+                </form> */}
+
+<Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
+    
+            <Form className={classes.root} noValidate autoComplete="off">
+                <div>
+                <Field 
+                    id="name"
+                    type="text" 
+                    name="name" 
+
+                    />
+                    {formik.touched.name && formik.errors.name ? (
+                     <Alert severity="error">{formik.errors.name}</Alert>
+
+                    ):(null)}
+                </div>
+                <div>
+                <Field 
+                    type="email"
+                    id="email" 
+                    name="email"
+
+                     />
+                     {formik.touched.email && formik.errors.email ? (
+                     <Alert severity="error">{formik.errors.email}</Alert>
+
+                    ):(null)}
+                </div>
+                <div>
+                <Field 
+                    type="text" 
+                    id="channel" 
+                    name="channel"
+                 />
+                    {formik.touched.channel && formik.errors.channel ? (
+                     <Alert severity="error">{formik.errors.channel}</Alert>
+                    ):(null)}
+                
+                </div>
+                <Button type="submit"  variant="contained" color="secondary" fullWidth={true} >Submit</Button>   
+                </Form>
+
+
+</Formik>
+
         </div>
     )
 }
